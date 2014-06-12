424a7f3f17c18cd5565bb87d89d2c0989e2dacf4
akalie <askaslie@gmail.com>
2014-06-12 21:21:09 +0400

    crm

---
+++ index.php
@@ -0,0 +1,370 @@
+<?php header ("Content-Type: text/html; charset=utf-8"); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- Sites-Stroy.ru by iProger -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Часы «СПИДОМЕТР» - Наручные LED</title>
+	<link media="all" rel="stylesheet" type="text/css" href="css/style.css" />
+	<link media="all" rel="stylesheet" type="text/css" href="css/jquery.countdown.css" />
+	<link media="all" rel="stylesheet" type="text/css" href="css/gallery.css" />
+	<script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+	<script src="js/jquery.countdown.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+    <LINK rel="icon" href="images/favicon.png" type="image/x-icon">
+</head>
+<body>
+	<div class=wrapper>
+		<div class=header>
+            <div class=phone>
+                <!--Наш телефон<br/><br/>
+                (499) 704-39-82-->
+            </div>
+			<div class=main>
+				<div class=htext>
+					<img src="images/logo.png" /><br/>
+					Оригинальные бинарные LED часы Racer  «Спидометр»
+				</div>
+				<div class=himg><img src="images/clock.png"/></div>
+				<div class=block-exclusive>
+					<div class=ex-head></div>
+					<div class=ex-body>
+						<div class=stock-price></div>
+						<small>Заполните пожалуйста поля</small>
+						<form class=sval-form method=POST action="ajax/order.php" target="speedy">
+						<input name=fio type=text sval="ФИО" value="ФИО"/>
+						<input name=email type=text sval="E-mail" value="E-mail"/>
+						<input id="tel" name=tel type=text sval="+7(___) ___-__-__" value="+7(___) ___-__-__"/>
+						<input name=adress type=text sval="Адрес (индекс, город и т. д.)" value="Адрес (индекс, город и т. д.)"/>
+						<button class="buy">КУПИТЬ</button>
+						</form>
+						<br/>
+					</div>
+				</div><br/><br/><br/><br/><br/><br/>
+				</div>
+			</div>
+		</div>
+		<div class=stock-time>
+			<div class=main>
+				<div class=banner>
+					<img src="images/banner-time.png" /><br/>
+					<span>Осталось 17 часов со скидкой</span>
+					
+				</div>
+				<div class=timer>
+					<div id="countdown"></div>
+					<div class="days">Дни</div>
+					<div class="hours">Часы</div>
+					<div class="mins">Минуты</div>
+					<div class="secs">Секунды</div>
+					<div class=clear></div>
+				</div>
+				<div class=clear></div>
+			</div>
+		</div>
+		<div class=features>
+			<div class=main>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-1.png"/></div>
+					<div class=ftext>
+						<h3>Оригинальный дизайн</h3>
+						Бинарные LED часы имеют оригинальный дизайн, время отображается в виде спидометра.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-2.png"/></div>
+					<div class=ftext>
+						<h3>Красивая подсветка</h3>
+                        На циферблате бинарных часов расположены белые и синие светодиоды. Белые обозначают часы, а синие - минуты.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=feat>
+					<div class=fimg><img src="images/feat-3.png"/></div>
+					<div class=ftext>
+						<h3>Просты в использовании</h3>
+						Отображение времени происходит по нажатию верхней кнопки на правой боковой панели часов, 
+                        отображение даты по двойному нажатию верхней кнопки.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=clear></div>
+			</div>
+		</div>
+		<div class=about>
+			<div class=main>
+				<h1>Часы "Спидометр"</h1>
+				<div class=video>
+
+				<iframe width="449" height="264" src="http://vk.com/video_ext.php?oid=246376291&id=168739941&hash=75d5213969e122c7&hd=1" frameborder="0" allowfullscreen></iframe>
+				</div>
+				<div class=info>
+                    Наручные часы спидометр понравятся всем любителям спортивных автомобилей. Источником вдохновения при разработке дизайна часов послужила приборная панель 
+					автомобиля. При нажатии на левую верхнюю кнопку часы отображают время весьма необычным и оригинальным способом. Часы отображаются на верхнем светодиодном 
+					циферблате, а минуты на нижнем. Эти два основных циферблата также служат для отображения текущей даты. На двух небольших элементах слева от циферблатов 
+					отображается время суток и день недели. Бинарные часы спидометр оснащены яркими светодиодами, которые будут отлично видны как ночью, так и днем. 
+					Водонепроницаемый корпус часов изготовлен из нержавеющей стали, обладает значительной прочностью и запоминающимся дизайном. 
+					<div class=gallery style="margin-bottom:20px;">
+						<div class=gallery_container>
+							<img src="images/1.jpg" class='gallery_main animation in'>
+						</div>
+						<div class=gallery_thumbnails>
+						<div class=gallery_thumbnail><img src="images/1.jpg"></div>
+							<div class=gallery_thumbnail><img src="images/2.jpg"></div>
+							<div class=gallery_thumbnail><img src="images/3.jpg"></div>
+							<div class=gallery_thumbnail><img src="images/4.jpg"></div>
+						</div>				
+					</div>
+				</div>
+				<div class=clear></div><br/>
+				<center><a href="#buy"><button class=bynow>Купить сейчас</button></a></center>
+			</div>
+		</div>
+		<div class=benefits>
+			<div class=main>
+				<h1>Преимущества бинарных LED часов Racer (Спидометр):</h1>
+				<div class=center>
+					<ul style="margin-right:180px;">
+						<li>водонепроницаемый и крепкий корпус из нержавеющей стали</li>
+						<li>удобный и стильный браслет из полиуретана</li>
+						<li>яркая светодиодная подсветка, которая позволяет точно определить время как днем, так и ночью</li>
+					</ul>
+					<ul>
+						<li>высокое качество изготовления и высокая точность</li>
+						<li>Бинарные LED часы Racer (Спидометр) отличный подарок себе и близким</li>
+						<li>Радость каждому купившему эти часы</li>
+					</ul>
+					<div class=clear></div>
+					<div class=rarrows></div>
+					<img src="images/ok.png" align="left" />
+					<div class=oktext>
+						<h2>LED часы — отличный подарок себе и близким</h2>
+						Пожалуй, основным назначением часов будет не только отображение времени, но и подчеркивание стиля, имиджа, 
+						интересов и образа жизни владельца этого аксессуара. А оригинальный дизайн и необычный способ отображения 
+						времени вызовут интерес у коллег, друзей и знакомых. Они могут стать отличным подарком как для Вас, так и 
+						для близкого Вам человека. А если человек живет скоростью, то он будет просто в восторге от такого подарка, 
+						ведь этот аксессуар будет в полной мере подчеркивать его стиль жизни.
+					</div>
+					<div class=clear></div>
+ 				</div>
+				<br/>
+				<center><a href="#buy"><button class=bynow>Купить сейчас</button></a></center>
+			</div>
+		</div>
+		<div class=reviews>
+			<div class=main>
+				<h1>Отзывы наших счастливых клиентов</h1>
+				<div class=review>
+					<div class=avatar><img src="images/ava-1.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Сергей Егоров</strong><br/>
+							<small>г. Москва</small>
+						</div>
+						Реально классные бинарные часы. За часы спидометр и подарочную коробку - СПАСИБО!
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-2.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Владислав Ильин</strong><br/>
+							<small>г. Пермь</small>
+						</div>
+						Спасибо сыну очень часы понравились 
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-5.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Ирина Прокопьева</strong><br/>
+							<small>г. Мурманск</small>
+						</div>
+						Спасибо подарила мужу ему он очень доволен.
+					</div>
+					<div class=clear></div>
+				</div>
+				<div id=hide-rev style="display:none;">
+				<div class=review>
+					<div class=avatar><img src="images/ava-4.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Сергей Стерхов</strong><br/>
+							<small>г. Подольск </small>
+						</div>
+						Все красиво, и проектор и упаковка.Спасибо! И правда, звезды очень отчетливо видны!
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-3.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Юра Андреев</strong><br/>
+							<small>г. Екатеринбург</small>
+						</div>
+						Получил часы за короткий промежуток, всего 8 дней. Подарил своему племяннику, он в восторге. Спасибо!
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-6.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Павел Андреев</strong><br/>
+							<small>г. Казань</small>
+						</div>
+                       До Казани посылка дошла за 8 дней. Интересный проектор. В целом своих денег стоит.
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-10.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Анна Кашина</strong><br/>
+							<small>г. Санкт-Петербург</small>
+						</div>
+                         Прекрасный подарок детишкам моя дочка в восторге спасибо.
+					</div>
+					<div class=clear></div>
+				</div>
+								<div class=review>
+					<div class=avatar><img src="images/ava-8.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Саша Александров</strong><br/>
+							<small>г. Нижний Новгород</small>
+						</div>
+                       Очень классные часики спасибки вам
+					</div>
+					<div class=clear></div>
+				</div>
+				<div class=review>
+					<div class=avatar><img src="images/ava-7.jpg"/></div>
+					<div class=rtext>
+						<div class=infou>
+							<strong>Веталий Сергеев</strong><br/>
+							<small>г. Симферополь</small>
+						</div>
+                       Прекрасные часы , много функций , очень удобно !
+					</div>
+					<div class=clear></div>
+				</div>
+				</div>
+				<span onclick="showRev(this);" style="cursor:pointer; color:#049fbd;">Показать ещё >></span>
+			</div>
+		</div>
+		<div class=our-benefits>
+			<div class=main>
+				<h1>Наши преимущества</h1>
+				<div class="center2">
+				<div class=oben>
+					<img src="images/garant.png" />
+					<span class=obname>100% гарантия возврата денег.</span>
+					<div class=obdesc>Товар устроит вас или мы вернем вам деньги!</div>
+				</div>
+				<div class=oben>
+					<img src="images/bmap.png" />
+					<span class=obname>Доставка во все города России</span>
+					<div class=obdesc></div>
+				</div>
+				<div class=oben style="margin-right:0px;">
+					<img src="images/econom.png" />
+					<span class=obname>Низкие цены</span>
+					<div class=obdesc>Мы работаем напрямую с производителем,
+					поэтому можем позволить себе акции и
+					низкие цены.</div>
+				</div>
+				<div class=clear></div>
+				</div>
+			</div>
+		</div>
+		<div class=howjob>
+			<div class=main>
+				<h1>Как мы работаем</h1>
+				<center>
+					<div class=steps>
+						<div class=step1>
+							<span>Вы оставляете заявку</span><br/>
+							Вы оставляете заявку на сайте или по телефону
+						</div>
+						<div class=step2>
+							<span>Мы доставляем Вашу посылку</span><br/>
+							Мы быстро доставляем Вашу посылку по указанному адресу
+						</div>
+						<div class=step3>
+							<span>Наш менеджер связывается с вами</span><br/>
+							Менеджер связывается с Вами для <br/>подтверждения заказа
+						</div>
+						<div class=step4>
+							<span>Вы получаете свой товар</span><br/>
+							Вы получаете свой товар и платите <br/>за него на почте или курьеру
+						</div>
+					</div>
+				</center>
+			</div>
+		</div>
+		<div id=buy class=footer>
+		    <div class=main>
+				<center>
+				<h1>Купи <span style="codlor:red;">Часы "Спидометр"</span> сегодня!</h1><br/><br/>	
+				<form class=sval-form method=POST action="ajax/order.php" target="speedy">
+				<input name=fio type=text sval="ФИО" value="ФИО" />
+				<input name=email type=text sval="E-mail" value="E-mail"/>
+				<input id="tel2" name=tel type=text sval="+7(___) ___-__-__" value="+7(___) ___-__-__"/>
+				<input name=adress type=text sval="Адрес (индекс, город и т. д.)" value="Адрес (индекс, город и т. д.)"/>
+				<br/>
+				<button class="buy">Купить сейчас</button>
+				</form>
+				</center>
+			</div>
+		</div>
+		<center class=copy>ООО "КУРЬЕР" ОГРН: 1137746208049 ИНН: 7725785302 КПП: 772501001 ОКПО: 17277510 Юр. Адрес: 115280, г. Москва, Автозаводская ул., дом 17, корп. 3, офис 11</center>
+	</div>
+	<div id="suc_order" class="modal_window"> 
+		<div class="close"></div>
+		<p>Мы отправили на ваш email письмо с данными заказа, если вы его там не нашли - посмотрите в спаме.</p>
+	</div>
+	<div id="scroller" class="b-top" style="display: none;"><span class="b-top-but"></span></div>
+   <div class="metrics" style="display: none">
+       <!-- Yandex.Metrika informer -->
+       <a href="https://metrika.yandex.ru/stat/?id=24961082&amp;from=informer"
+          target="_blank" rel="nofollow"><img src="//bs.yandex.ru/informer/24961082/3_1_FFFFFFFF_EFEFEFFF_0_pageviews"
+                                              style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try{Ya.Metrika.informer({i:this,id:24961082,lang:'ru'});return false}catch(e){}"/></a>
+       <!-- /Yandex.Metrika informer -->
+
+       <!-- Yandex.Metrika counter -->
+       <script type="text/javascript">
+           (function (d, w, c) {
+               (w[c] = w[c] || []).push(function() {
+                   try {
+                       w.yaCounter24961082 = new Ya.Metrika({id:24961082,
+                           webvisor:true,
+                           clickmap:true,
+                           trackLinks:true,
+                           accurateTrackBounce:true});
+                   } catch(e) { }
+               });
+
+               var n = d.getElementsByTagName("script")[0],
+                   s = d.createElement("script"),
+                   f = function () { n.parentNode.insertBefore(s, n); };
+               s.type = "text/javascript";
+               s.async = true;
+               s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+               if (w.opera == "[object Opera]") {
+                   d.addEventListener("DOMContentLoaded", f, false);
+               } else { f(); }
+           })(document, window, "yandex_metrika_callbacks");
+       </script>
+       <noscript><div><img src="//mc.yandex.ru/watch/24961082" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+       <!-- /Yandex.Metrika counter -->
+   </div>
+</body>
+</html>
